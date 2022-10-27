74c0217529135cf1a2ecc7bd429d7a3b05eff5fc
Daniel Kogan <daniel.kogan@materna.de>
2022-10-27 13:44:46 +0200

    1.0.14

--- src/math/hash.ts
+++ src/math/hash.ts
@@ -1,23 +1,25 @@
 export const cyrb53 =
   (seed = 0) =>
-    (str: string) => {
-      let h1 = 0xdeadbeef ^ seed;
-      let h2 = 0x41c6ce57 ^ seed;
-      for (let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-      }
+  (str: string) => {
+    let h1 = 0xdeadbeef ^ seed;
+    let h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
 
-      h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-      h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
 
-      return 4294967296 * (2097151 & h2) + (h1 >>> 0);
-    };
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+  };
 
-export function cyrb128(str: string): [number, number, number, number]  {
-  let h1 = 1779033703, h2 = 3144134277,
-    h3 = 1013904242, h4 = 2773480762;
+export function cyrb128(str: string): [number, number, number, number] {
+  let h1 = 1779033703,
+    h2 = 3144134277,
+    h3 = 1013904242,
+    h4 = 2773480762;
   for (let i = 0, k; i < str.length; i++) {
     k = str.charCodeAt(i);
     h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
--- src/math/random.ts
+++ src/math/random.ts
@@ -1,4 +1,4 @@
-import {cyrb128} from "./hash";
+import { cyrb128 } from './hash';
 
 /**
  * @param a: seed part 1
@@ -6,7 +6,7 @@ import {cyrb128} from "./hash";
  * @param c: seed part 1
  * @param d: seed part 1
  */
-export function sfc32(a: number, b: number, c: number, d: number){
+export function sfc32(a: number, b: number, c: number, d: number) {
   a >>>= 0;
   b >>>= 0;
   c >>>= 0;
@@ -14,17 +14,17 @@ export function sfc32(a: number, b: number, c: number, d: number){
   let t = (a + b) | 0;
   return function () {
     t = (a + b) | 0;
-    a = b ^ b >>> 9;
-    b = c + (c << 3) | 0;
-    c = (c << 21 | c >>> 11);
-    d = d + 1 | 0;
-    t = t + d | 0;
-    c = c + t | 0;
+    a = b ^ (b >>> 9);
+    b = (c + (c << 3)) | 0;
+    c = (c << 21) | (c >>> 11);
+    d = (d + 1) | 0;
+    t = (t + d) | 0;
+    c = (c + t) | 0;
     return (t >>> 0) / 4294967296;
-  }
+  };
 }
 
 export function randomSfc32(seed: string) {
-  const [a, b, c, d] = cyrb128(seed)
-  return sfc32(a, b, c, d)
+  const [a, b, c, d] = cyrb128(seed);
+  return sfc32(a, b, c, d);
 }
